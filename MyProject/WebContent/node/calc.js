335753977ff4522221e8ab2cf17ad7a33ace3c61
bit <bit@s20.java48.com>
2014-03-13 19:17:08 +0900

    node test

---
+++ MyProject/WebContent/node/calc.js
@@ -0,0 +1,29 @@
+var http = require('http');
+http.createServer(function (req, res) {
+	
+  console.log(req.url);
+  var urlObj = require('url').parse(req.url, true);
+  var pathname = urlObj.pathname;
+  var params = urlObj.query;
+ try{ 
+  if(pathname == '/calc'){
+	  var result = 0;
+	  var a = Number(params.a);
+	  var b = Number(params.b);
+	  if(params.op == 'plus'){
+		result = a + b;
+	  }else if(params.op == 'minus'){
+		  result = a - b;
+	  }else{
+		  result = '지원하지 않는 연사자이거나 또는 값이 옳지 않습니다.';
+	  }
+	  res.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
+	  res.write(String(result));
+
+  }
+ } catch (err){
+	 res.write(String(err));
+ }
+  res.end();
+}).listen(8882, '192.168.200.63');
+console.log('Server running at http://127.0.0.1:8882/');
